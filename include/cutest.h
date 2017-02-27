0ad08633e73bdf78e43622d09270a26e085512f2
Martin Mitas <mity@morous.org>
2017-02-27 14:16:42 +0100

    Update copyright notes (add year 2017).

--- include/cutest.h
+++ include/cutest.h
@@ -2,7 +2,7 @@
  * CUTest -- C/C++ Unit Test facility
  * <http://github.com/mity/cutest>
  *
- * Copyright (c) 2013-2016 Martin Mitas
+ * Copyright (c) 2013-2017 Martin Mitas
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
