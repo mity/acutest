bb90b47ab3bf87a894bc462e35eae75e1a0000a5
Kevin Sjöberg <mail@kevinsjoberg.com>
2021-06-21 10:31:50 +0200

    Add support for macOS debugging

--- include/acutest.h
+++ include/acutest.h
@@ -306,6 +306,15 @@
     #include <io.h>
 #endif
 
+#if defined(__APPLE__)
+    #define ACUTEST_MACOS_
+    #include <assert.h>
+    #include <stdbool.h>
+    #include <sys/types.h>
+    #include <unistd.h>
+    #include <sys/sysctl.h>
+#endif
+
 #ifdef __cplusplus
     #include <exception>
 #endif
@@ -1629,6 +1638,36 @@ acutest_is_tracer_present_(void)
 }
 #endif
 
+#ifdef ACUTEST_MACOS_
+static bool
+AmIBeingDebugged(void)
+{
+    int junk;
+    int mib[4];
+    struct kinfo_proc info;
+    size_t size;
+
+    // Initialize the flags so that, if sysctl fails for some bizarre
+    // reason, we get a predictable result.
+    info.kp_proc.p_flag = 0;
+
+    // Initialize mib, which tells sysctl the info we want, in this case
+    // we're looking for information about a specific process ID.
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_PROC;
+    mib[2] = KERN_PROC_PID;
+    mib[3] = getpid();
+
+    // Call sysctl.
+    size = sizeof(info);
+    junk = sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, NULL, 0);
+    assert(junk == 0);
+
+    // We're being debugged if the P_TRACED flag is set.
+    return ( (info.kp_proc.p_flag & P_TRACED) != 0 );
+}
+#endif
+
 int
 main(int argc, char** argv)
 {
@@ -1691,6 +1730,10 @@ main(int argc, char** argv)
 #endif
 #ifdef ACUTEST_LINUX_
             if(acutest_is_tracer_present_())
+                acutest_no_exec_ = 1;
+#endif
+#ifdef ACUTEST_MACOS_
+            if(AmIBeingDebugged())
                 acutest_no_exec_ = 1;
 #endif
 #ifdef RUNNING_ON_VALGRIND
